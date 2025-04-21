9baa4ccb241e6a32390bab965648e2b0b56ec5fc
Arkadiusz 'Amok' Stanek <stanek.arkadiusz@gmail.com>
2025-04-21 11:27:00 +0200

    - new folder: Controls

---
+++ Controls/FiszkaCard.xaml.cs
@@ -0,0 +1,16 @@
+namespace Fiszki.Controls;
+
+public partial class FiszkaCard : ContentView
+{
+
+	public static readonly BindableProperty FiszkaObjectProperty = BindableProperty.Create(nameof(FiszkaObject), typeof(Fiszka), typeof(FiszkaCard), null);
+	public Fiszka FiszkaObject
+	{
+		get => (Fiszka)GetValue(FiszkaObjectProperty);
+		set => SetValue(FiszkaObjectProperty, value);
+    }
+	public FiszkaCard()
+	{
+		InitializeComponent();
+    }
+}
