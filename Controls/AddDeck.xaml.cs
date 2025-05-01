dde2691d88150abac547a5e6dc7648d1551e58fc
Arkadiusz 'Amok' Stanek <stanek.arkadiusz@gmail.com>
2025-05-01 20:59:31 +0200

    - added visual for AddDeckControl

--- Controls/AddDeck.xaml.cs
+++ Controls/AddDeck.xaml.cs
@@ -6,4 +6,16 @@ public partial class AddDeck : ContentView
 	{
 		InitializeComponent();
 	}
+
+    private void PointerGestureRecognizer_PointerEntered(object sender, PointerEventArgs e)
+    {
+        var hasStyle = Resources.TryGetValue("BorderOnHover", out object style);
+        DeckBorder.Style = (Style)style;
+    }
+
+    private void PointerGestureRecognizer_PointerExited(object sender, PointerEventArgs e)
+    {
+        var hasStyle = Resources.TryGetValue("BorderNormal", out object style);
+        DeckBorder.Style = (Style)style;
+    }
 }
