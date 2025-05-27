582f45ea9eb7a8fd8878ae5c49f6574671f0312b
Arkadiusz 'Amok' Stanek <stanek.arkadiusz@gmail.com>
2025-05-27 20:20:46 +0200

    - added new example deck

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -74,6 +74,30 @@ namespace Fiszki
             Verbs.AddFiszka(new Fiszka("dire", "mówić"));
             Verbs.AddFiszka(new Fiszka("venir", "przychodzić, przyjeżdżać"));
             Verbs.AddFiszka(new Fiszka("manger", "jeść"));
+
+            FiszkaDeck.AddDeck("périodes de la vie");
+            FiszkaDeck Periodes = FiszkaDeck.AllDecks[0];
+            Periodes.AddFiszka(new Fiszka("un nouveau-né", "noworodek"));
+            Periodes.AddFiszka(new Fiszka("un bébé", "niemowlę"));
+            Periodes.AddFiszka(new Fiszka("un(e) enfant", "dziecko"));
+            Periodes.AddFiszka(new Fiszka("un(e) adolescent(e)", "nastolatek (-tka)"));
+            Periodes.AddFiszka(new Fiszka("un(e) adulte", "osoba dorosła"));
+            Periodes.AddFiszka(new Fiszka("un jeune homme", "młody dorosły"));
+            Periodes.AddFiszka(new Fiszka("une jeune femme", "młoda kobieta"));
+            Periodes.AddFiszka(new Fiszka("une personne âgée", "osoba starsza"));
+            Periodes.AddFiszka(new Fiszka("une enfance", "dzieciństwo"));
+            Periodes.AddFiszka(new Fiszka("une adolescence", "wiek nastoletni, okres dojrzewania"));
+            Periodes.AddFiszka(new Fiszka("une jeunesse", "młodość"));
+            Periodes.AddFiszka(new Fiszka("une vieillesse", "starość"));
+            Periodes.AddFiszka(new Fiszka("grandir", "dorastać"));
+            Periodes.AddFiszka(new Fiszka("vieillir", "starzeć się"));
+            Periodes.AddFiszka(new Fiszka("avoir ... an(s)", "mieć ... lat"));
+            Periodes.AddFiszka(new Fiszka("être mineur(e)", "być niepełnoletnim"));
+            Periodes.AddFiszka(new Fiszka("être majeur(e)", "być pełnoletnim"));
+            Periodes.AddFiszka(new Fiszka("fêter son anniversaire", "obchodzić / świętować urodziny"));
+            Periodes.AddFiszka(new Fiszka("élever un enfant", "wychowywać dziecko"));
+            Periodes.AddFiszka(new Fiszka("gâter un enfant ", "rozpieszczać dziecko"));
+
         }
 
     }
