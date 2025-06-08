5c3b16297d98b5caec80c2ffd876f07641548109
Arkadiusz 'Amok' Stanek <stanek.arkadiusz@gmail.com>
2025-06-08 21:10:36 +0200

    - LearnSetupPage foundation work

--- Pages/LearnSetupPage.xaml.cs
+++ Pages/LearnSetupPage.xaml.cs
@@ -8,18 +8,42 @@ using Fiszki.Data;
 namespace Fiszki;
 
 public partial class LearnSetupPage : ContentPage
-{   
-	
-	public LearnSetupPage()
+{
+    public static ObservableCollection<FiszkaDeck> selectedDecks;
+
+    public LearnSetupPage()
 	{
 		InitializeComponent();
 
 		DeckCollectionView.ItemsSource = FiszkaDeck.AllDecks;
+
     }
 
-    private void StartSessionButton_Clicked(object sender, EventArgs e)
+    private async void StartSessionButton_Clicked(object sender, EventArgs e)
     {
-		int howManyCards;
+        
+        if (DeckCollectionView.SelectedItems.Count == 0)
+		{
+            await DisplayAlert(
+                      "Brak fiszek do nauki",
+                      "Musisz wybrać conajmniej jedną talię!",
+                      "Ok");
+        }
+
+        else
+        {
+            FiszkaDeck g = DeckCollectionView.SelectedItems[0] as FiszkaDeck;
+
+            await DisplayAlert(
+                      "Brak fiszek do nauki",
+                      g.Name,
+                      "Ok");
+        }
+
+
+
+
+        int howManyCards;
 		Random r = new Random();
 		
 
---
+++ Pages/NewLearnPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿
+
+using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
+using Fiszki.Data;
+
+namespace Fiszki;
+
+public partial class NewLearnPage : ContentPage
+{   
+	
+    public static ObservableCollection<FiszkaDeck> selectedDecks;
+    ObservableCollection<Fiszka> learningDeck = new ObservableCollection<Fiszka>();
+    int previous_n;
+	Fiszka? currentFiszka;
+	
+	public NewLearnPage()
+	{
+		InitializeComponent();
+        fullFiszkaControl.IsEditable = false;
+        selectedDecks = FiszkaDeck.AllDecks;
+		MixDecks();
+        fullFiszkaControl.IsVisible = false;
+        ScoreButtons.IsVisible = false;
+
+        if (CheckIfNotEmpty())
+		{
+         halfFiszkaControl.FiszkaObject = ChooseRandomFiszka(learningDeck);
+        }
+
+        
+
+    }
+
+
+	private bool CheckIfNotEmpty()
+	{
+		if (learningDeck.Count > 0) return true;
+		else return false;
+
+	}
+
+	private void MixDecks()
+	{
+		foreach (FiszkaDeck deck in selectedDecks)
+		{
+			foreach (Fiszka fiszka in deck.Deck)
+			{
+				learningDeck.Add(fiszka);
+			}
+		}
+
+	}
+
+    private Fiszka ChooseRandomFiszka(ObservableCollection<Fiszka> deck)
+	{
+		var r = new Random();
+		int n = r.Next(0, deck.Count() - 1);
+		while (n == previous_n)
+		{
+			n = r.Next(0, deck.Count() - 1);
+        }
+		previous_n = n;
+        currentFiszka = deck[n];
+		return deck[n];
+	}
+
+    private void Refresh(object sender, EventArgs e)
+    {
+		fullFiszkaControl.IsVisible = false;
+		halfFiszkaControl.IsVisible = true;
+		ScoreButtons.IsVisible = false;
+
+        halfFiszkaControl.FiszkaObject = ChooseRandomFiszka(learningDeck);
+		halfFiszkaControl.ShowRandomPhrase();
+
+    }
+
+    private void ShowClicked(object sender, EventArgs e)
+    {
+		fullFiszkaControl.FiszkaObject = currentFiszka;
+		fullFiszkaControl.IsVisible = true;
+		halfFiszkaControl.IsVisible = false;
+		ScoreButtons.IsVisible = true;
+    }
+
+	private void ScoreClicked(object sender, EventArgs e)
+    {
+		var param = ((Button)sender).CommandParameter;
+		double score = Double.Parse((string)param);
+		currentFiszka.AddScore(score);
+		Refresh(0, null);
+	}
+
+
+}
