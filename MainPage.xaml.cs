f555466723b56329709c911fdd7e77faa28edb16
Arkadiusz 'Amok' Stanek <stanek.arkadiusz@gmail.com>
2025-06-30 20:58:07 +0200

    Added new decks for layout and learning testing

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -110,6 +110,70 @@ namespace Fiszki
             Periodes.AddFiszka(new Fiszka("élever un enfant", "wychowywać dziecko"));
             Periodes.AddFiszka(new Fiszka("gâter un enfant ", "rozpieszczać dziecko"));
 
+            FiszkaDeck.AddDeck("travail");
+            FiszkaDeck Travail = FiszkaDeck.AllDecks[0];
+            Travail.AddFiszka(new Fiszka("une entreprise", "firma"));
+            Travail.AddFiszka(new Fiszka("une société", "firma"));
+            Travail.AddFiszka(new Fiszka("un(e) employé(e)", "pracownik"));
+            Travail.AddFiszka(new Fiszka("un patron, une patronne", "szef, szefowa"));
+            Travail.AddFiszka(new Fiszka("un(e) société", "firma"));
+            Travail.AddFiszka(new Fiszka("collègue (de travail)", "kolega z pracy"));
+            Travail.AddFiszka(new Fiszka("un(e) client(e)", "klient(ka)"));
+            Travail.AddFiszka(new Fiszka("un(e) stagiaire", "stażysta (-tka)"));
+            Travail.AddFiszka(new Fiszka("faire un stage", "być na stażu"));
+            Travail.AddFiszka(new Fiszka("un rendez-vous", "spotkanie"));
+            Travail.AddFiszka(new Fiszka("une réunion", "zebranie"));
+            Travail.AddFiszka(new Fiszka("chercher un/du travail", "szukać pracy"));
+            Travail.AddFiszka(new Fiszka("postuler pour un poste", "ubiegać się o stanowisko"));
+            Travail.AddFiszka(new Fiszka("déposer sa candidature", "zgłosić swoją kandydature"));
+            Travail.AddFiszka(new Fiszka("passer un entretien d'embauche", "przejść rozmowę o pracę"));
+            Travail.AddFiszka(new Fiszka("avoir les compétences requises (f.)", "mieć wymagane kompetencje"));
+            Travail.AddFiszka(new Fiszka("avoir de l’expérience (f.)", "mieć doświadczenie"));
+            Travail.AddFiszka(new Fiszka("embaucher", "zatrudnić kogoś"));
+            Travail.AddFiszka(new Fiszka("employer", "zatrudnić kogoś"));
+            Travail.AddFiszka(new Fiszka("engager qqn", "zatrudnić kogoś"));
+            Travail.AddFiszka(new Fiszka("obtenir / quitter un travail", "dostać / rzucić pracę"));
+            Travail.AddFiszka(new Fiszka("signer / résilier un contrat", "podpisać / rozwiązać umowę"));
+            Travail.AddFiszka(new Fiszka("un contrat à durée déterminée (CDD)", "umowa na czas określony"));
+            Travail.AddFiszka(new Fiszka("un contrat à durée indéterminée (CDI)", "umowa na czas nieokreślony"));
+            Travail.AddFiszka(new Fiszka("travailler à temps plein", "pracować na pełen etat"));
+            Travail.AddFiszka(new Fiszka("travailler à mi-temps", "pracować na pół etat"));
+            Travail.AddFiszka(new Fiszka("travailler en intérim", "wykonywać pracę tymczasową"));
+            Travail.AddFiszka(new Fiszka("faire un remplacement", "pracować na zastępstwie"));
+            Travail.AddFiszka(new Fiszka("faire des heures (f.) supplémentaires", "brać nadgodziny"));
+            Travail.AddFiszka(new Fiszka("être en déplacement professionnel", "być na wyjeździe służbowym"));
+            Travail.AddFiszka(new Fiszka("toucher son salaire", "pobierać pensję"));
+            Travail.AddFiszka(new Fiszka("obtenir une augmentation de salaire", "dostać podwyżkę"));
+            Travail.AddFiszka(new Fiszka("ontenir une promotion", "otrzymać awans"));
+            Travail.AddFiszka(new Fiszka("prendre un congé", "wziąć urlop"));
+            Travail.AddFiszka(new Fiszka("licencier qqn", "zwolnić kogoś"));
+            Travail.AddFiszka(new Fiszka("être licencié(e)", "być zwolnionym"));
+            Travail.AddFiszka(new Fiszka("déposer un préavis", "złożyć wypowiedzenie"));
+            Travail.AddFiszka(new Fiszka("être au chômage", "być na bezrobociu"));
+            Travail.AddFiszka(new Fiszka("prendre sa retraite", "przejść na emeryturę"));
+
+            FiszkaDeck.AddDeck("amour relations");
+            FiszkaDeck Amour = FiszkaDeck.AllDecks[0];
+
+            Amour.AddFiszka(new Fiszka("un petit ami", "chłopak"));
+            Amour.AddFiszka(new Fiszka("un copain", "chłopak"));
+            Amour.AddFiszka(new Fiszka("une petite amie", "dziewczyna"));
+            Amour.AddFiszka(new Fiszka("une copine", "dziewczyna"));
+            Amour.AddFiszka(new Fiszka("un compagnon", "partner"));
+            Amour.AddFiszka(new Fiszka("une compagne", "partnerka"));
+            Amour.AddFiszka(new Fiszka("un fiancé", "narzeczony"));
+            Amour.AddFiszka(new Fiszka("une fiancée", "narzeczona"));
+            Amour.AddFiszka(new Fiszka("un jeune marié", "pan młody"));
+            Amour.AddFiszka(new Fiszka("une jeune mariée", "panna młoda"));
+            Amour.AddFiszka(new Fiszka("un témoin de mariage", "świadek, świadkowa"));
+            Amour.AddFiszka(new Fiszka("être amoureux (-euse) de qqn", "być zakochym(-ą) w kimś"));
+            Amour.AddFiszka(new Fiszka("s'aimer", "kochać się, darzyć się miłością"));
+            Amour.AddFiszka(new Fiszka("sortir ensemble", "chodzić ze sobą"));
+            Amour.AddFiszka(new Fiszka("être en couple avec qqn", "być z kimś w związku"));
+            Amour.AddFiszka(new Fiszka("être pacsé(e)", "być w formalnym związku partnerskim"));
+            Amour.AddFiszka(new Fiszka("sortir ensemble", "chodzić ze sobą"));
+            Amour.AddFiszka(new Fiszka("vivre en concubinage", "żyć w konkubinacie"));
+            Amour.AddFiszka(new Fiszka("demander qqn en mariage", "poprosić kogoś o rękę"));
         }
 
     }
